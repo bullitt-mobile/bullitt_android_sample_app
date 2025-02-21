f87a008ced2900d2e6944a99b02ac51b33feb2de
Karthik Ambu <karthik@bullitt.com>
2025-02-21 12:03:20 +0000

    Simplify ContentBundleExt.kt

--- app/src/main/java/com/bullitt/sampleapp/store/ContentBundleExt.kt
+++ app/src/main/java/com/bullitt/sampleapp/store/ContentBundleExt.kt
@@ -7,19 +7,27 @@ import com.bullitt.sdk.platform.data.smp.generateMessageId
 /** Convert a ContentBundle to a Message, for incoming messages */
 fun ContentBundle.toMessage(isOutgoing: Boolean): Message {
   val content = this.content
-  // Convert SmpContent to a Pair<partnerId, content>
-  val value: Pair<Long, String>? =
-    when (content) {
-      is TextContent -> Pair(content.partnerId, content.textMessage)
-      else -> Pair(-1, content.contentType.name)
-    }
 
-  return Message(
-    partnerNumber = value?.first,
-    content = value?.second ?: content.contentType.name,
-    isOutgoing = isOutgoing,
-    timestamp = this.smpHeader.eventTimestamp.timestampInMillis,
-    status = if (isOutgoing) Message.Status.SENDING else Message.Status.DELIVERED,
-    messageId = this.smpHeader.generateMessageId().toString(),
-  )
+  return when (content) {
+    is TextContent ->
+      Message(
+        partnerNumber = content.partnerId,
+        content = content.textMessage,
+        isOutgoing = isOutgoing,
+        timestamp = this.smpHeader.eventTimestamp.timestampInMillis,
+        status = if (isOutgoing) Message.Status.SENDING else Message.Status.DELIVERED,
+        messageId = this.smpHeader.generateMessageId().toString(),
+      )
+    // We don't care about other message types in this sample application, but this can be handled
+    // here
+    else ->
+      Message(
+        partnerNumber = -1,
+        content = content.contentType.name,
+        isOutgoing = isOutgoing,
+        timestamp = this.smpHeader.eventTimestamp.timestampInMillis,
+        status = if (isOutgoing) Message.Status.SENDING else Message.Status.DELIVERED,
+        messageId = this.smpHeader.generateMessageId().toString(),
+      )
+  }
 }
